74f1f8262e374ee5d6410dadaa18a15be88f75e0
Wongsathorn.P <Wongsathorn.P@miryninnovation.com>
2016-08-18 20:05:57 +0700

    Pass Level1

--- Assets/Scripts/SpaceshipMotor.cs
+++ Assets/Scripts/SpaceshipMotor.cs
@@ -2,75 +2,84 @@
 using UnityEngine;
 using System.Collections;
 
-public class SpaceshipMotor : MonoBehaviour, IRequireUserInput {
-	
-	// Property for Injection
-	public IUserInputProxy InputProxy { get; set;}
+public class SpaceshipMotor : MonoBehaviour, IRequireUserInput
+{
 
-	public float Speed;
-	public Vector2 Min;
-	public Vector2 Max;
+    // Property for Injection
+    public IUserInputProxy InputProxy { get; set; }
 
-	
-	// Update method is called once per frame
-	// Lets call vertical and horizontal movement separately
-	// MoveHorizontally ();
-	// MoveVertically ();
-
-	void Update () {
-
-		if (InputProxy == null)
-						return;
-	}
-	
-
-	// To read user input you shoud use static class Input
-	// to get the value of horizontal axis and save it to a variable
-	// var horizontal = InputProxy.GetAxis ("Horizontal");
-	// let calculate delta x
-	// var dx = Speed * Time.deltaTime * horizontal;
-	// and move the ship
-	// his.transform.Translate (dx, 0, 0);
-	// For Level2 we should do the boundary check
-	// if (CanMoveHorizontally (dx, horizontal)) 
-
-	void MoveHorizontally ()
-	{
-		throw new NotImplementedException ();
-	}
+    public float Speed;
+    public Vector2 Min;
+    public Vector2 Max;
 
 
-	// This depends on the new x position
-	// float newX = transform.position.x + dX;
-	// and the direction of the movement
-	// return   (horizontal<0 && newX> Min.x) || (horizontal>0 && newX<Max.x);
-	bool CanMoveHorizontally(float dX, float horizontal)
-	{
-		throw new NotImplementedException ();
-	}
+    // Update method is called once per frame
+    // Lets call vertical and horizontal movement separately
+    // MoveHorizontally ();
+    // MoveVertically ();
 
-	// To read user input you shoud use static class Input
-	// To get the value of a vertical axis and save it
-	// var vertical = InputProxy.GetAxis ("Vertical");
-	// now we need delta y to move the ship
-	// var dy = Speed * Time.deltaTime * vertical;
-	// And lets move the object
-	// this.transform.Translate (0, dy, 0);
-	// For Level 2 :)
-	// Lets check the boundary
-	// if (CanMoveVertically (dy, vertical)) 
+    void Update()
+    {
+        if (InputProxy == null)
+            return;
 
-	void MoveVertically ()
-	{
-		throw new NotImplementedException ();		
-	}
+        MoveHorizontally();
+        MoveVertically();
+    }
 
-	// this depends on new y position
-	// float newY = transform.position.y + dY;
-	// return   (vertical<0 && newY> Min.y) || (vertical>0 && newY<Max.y);
-	bool CanMoveVertically(float dY, float vertical)
-	{
-		throw new NotImplementedException ();
-	}
+
+    // To read user input you shoud use static class Input
+    // to get the value of horizontal axis and save it to a variable
+    // var horizontal = InputProxy.GetAxis ("Horizontal");
+    // let calculate delta x
+    // var dx = Speed * Time.deltaTime * horizontal;
+    // and move the ship
+    // his.transform.Translate (dx, 0, 0);
+    // For Level2 we should do the boundary check
+    // if (CanMoveHorizontally (dx, horizontal)) 
+
+    void MoveHorizontally()
+    {
+        var horizontal = InputProxy.GetAxis("Horizontal");
+        var dx = Speed * Time.deltaTime * horizontal;
+
+        this.transform.Translate(dx, 0f, 0f);
+    }
+
+
+    // This depends on the new x position
+    // float newX = transform.position.x + dX;
+    // and the direction of the movement
+    // return   (horizontal<0 && newX> Min.x) || (horizontal>0 && newX<Max.x);
+    bool CanMoveHorizontally(float dX, float horizontal)
+    {
+        throw new NotImplementedException();
+    }
+
+    // To read user input you shoud use static class Input
+    // To get the value of a vertical axis and save it
+    // var vertical = InputProxy.GetAxis ("Vertical");
+    // now we need delta y to move the ship
+    // var dy = Speed * Time.deltaTime * vertical;
+    // And lets move the object
+    // this.transform.Translate (0, dy, 0);
+    // For Level 2 :)
+    // Lets check the boundary
+    // if (CanMoveVertically (dy, vertical)) 
+
+    void MoveVertically()
+    {
+		var vertical = InputProxy.GetAxis("Vertical");
+		var dy = Speed * Time.deltaTime * vertical;
+		this.transform.Translate(0f , dy , 0f);
+    }
+
+    // this depends on new y position
+    // float newY = transform.position.y + dY;
+    // return   (vertical<0 && newY> Min.y) || (vertical>0 && newY<Max.y);
+    bool CanMoveVertically(float dY, float vertical)
+    {
+        throw new NotImplementedException();
+    }
 
 }
