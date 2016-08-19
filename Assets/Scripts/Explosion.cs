cfbbb1321a8cd3957208678113380aa4e5d91d6a
Wongsathorn.P <Wongsathorn.P@miryninnovation.com>
2016-08-19 11:41:30 +0700

    Pass Lv4

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -2,14 +2,13 @@
 using UnityEngine;
 using System.Collections;
 
-public class Explosion : MonoBehaviour {
-
-
-	// This method is executed after animation is finished
-	// Use Destroy() command to destroy the object e.g.
-	// Destroy(this.gameObject) to remove
-	void ExplosionEnd()
-	{
-		throw new NotImplementedException();
-	}
+public class Explosion : MonoBehaviour
+{
+    // This method is executed after animation is finished
+    // Use Destroy() command to destroy the object e.g.
+    // Destroy(this.gameObject) to remove
+    void ExplosionEnd()
+    {
+		Destroy(this.gameObject);
+    }
 }
